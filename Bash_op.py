e2f21fda2258fb6531436695cc10b47e50b60cc1
Derector8 <110360198+Derector8@users.noreply.github.com>
2024-06-13 22:55:20 +0300

    The 1 version of 28.1 hw
    Added bash operator with .sh script to load data from API.
    Added Postgres operator to load data into table.
    Added Sensor to check if data in table.
    Added Email operator to send messages myself to gmail.

---
+++ Bash_op.py
@@ -0,0 +1,22 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id='load_regions_from_covid_API_dag',
+    start_date=pendulum.today(),
+    schedule=None,
+    tags=['hw 28.1']
+) as dag:
+    start_op = EmptyOperator(task_id='start')
+
+    load_covid_regions_op = BashOperator(
+        task_id='load_covid_regions',
+        bash_command='load_covid_regions.sh',
+        env={'message': 'Loading data from covid API'}
+    )
+
+    finish_op = EmptyOperator(task_id='finish')
+
+    start_op >> load_covid_regions_op >> finish_op
